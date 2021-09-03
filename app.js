b961a6984195af6426c8c4e9beb3931b8cc8f084
Rhona Lam <81948172+rhonall@users.noreply.github.com>
2021-09-03 21:34:06 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+const start = document.querySelector('#start')
+
+let result = 0
+let hitPosition
+let currentTime = 30
+let timeId = null
+let countDownId
+
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+
+function moveMole() {
+    timeId = setInterval(randomSquare, 600)
+}
+
+start.addEventListener('click', () => {
+    moveMole()
+    countDownId = setInterval(countDown, 1000)
+})
+
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0){
+        clearInterval(countDownId)
+        clearInterval(timeId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+}
+
+
+
